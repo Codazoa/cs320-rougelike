2e23e91c7d5c215eeacd20a6ea1cb0b15dbcede0
Cody.Vernon <cody.vernon@wsu.edu>
2022-01-20 14:01:14 -0800

    Adding .gitignore and moving some files

--- prototypes/data_scraping/game-files/gcdot-data-test.gd
+++ prototypes/data_scraping/game-files/gcdot-data-test.gd
@@ -5,7 +5,7 @@ func _ready():
 #	var exitCode = OS.execute("python3", ["testpy.py"], true, output) #test for running python script
 
 	var label = get_node("colorLabel") #select node colorLabel and set it to the variable
-	var color = ImportData.color_data["item6"] #calling in color_data from ImportData script and grabbing item1
+	var color = ImportData.color_data["item8"] #calling in color_data from ImportData script and grabbing item1
 	#var color = ImportData.color_data["item3"] #calling in color_data from ImportData script and grabbing item1
 	
 	print("Color", color)
