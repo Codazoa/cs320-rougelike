376ef1d598896f12acaaa0c3828112904b108dda
FinnishArmy <40931307+FinnishArmy@users.noreply.github.com>
2022-02-17 13:33:52 -0800

    Added metadata file directory
    Added a metadata file directory that will store the data of each NFT that gets created. 
    
    This is just a sample that was added. The image can be called upon by a directory that holds a screenshot of the final form of the character.
    Right now it is just holding sample information.
    
    Next to be done:
    Figure out a way for the smart contract to call the metadata.

---
+++ prototypes/nft/nft-mix/metadata/sample_metadata.py
@@ -0,0 +1,11 @@
+metadata_playerMinting = {
+    "name": "Player0",
+    "description": "First Player NFT Test",
+    "image": "https://www.newsbtc.com/wp-content/uploads/2021/11/Picture78.png",
+    "attributes": [{"trait_type": "empid", "value": 0}
+                    {"trait_type": "name", "value": cat_boat},
+                    {"trait_type": "playDate", "value": 02/17/2022},
+                    {"trait_type": "connectedParts", "value": None},
+                    {"trait_type": "numberOfDeaths", "value": 4}
+                    ],
+}
