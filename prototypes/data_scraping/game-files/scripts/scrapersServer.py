8f5fc5252b5509e6c9bcb02e50b19e88a47757da
codazoa <codazoa@gmail.com>
2022-02-21 20:51:39 -0800

    Getting api keys set up for weather data retrieval

--- prototypes/data_scraping/game-files/scripts/scrapersServer.py
+++ prototypes/data_scraping/game-files/scripts/scrapersServer.py
@@ -1,7 +1,9 @@
-import os, zmq, time
+import os, zmq, time, random, json
 from datetime import datetime as dt
 from PIL import Image
 from bs4 import BeautifulSoup
+
+import config
 
 class DotScraper():
     def __init__(self):
@@ -13,9 +15,10 @@ class DotScraper():
         hr = dateTime.hour
         min = dateTime.minute
         self.lastAccess = (hr, min)
+        return((hr, min))
 
     def updateDot(self): # download the new image of the dot, store in ../data/ as dot.png
-        page = BeautifulSoup
+        # page = BeautifulSoup
         self.__accessTime(dt.now())
         pass
 
@@ -28,29 +31,40 @@ class DotScraper():
 
 class CityTempScraper():
     def __init__(self):
-        self.tempURLs = '' # dict of city names and urls
+        self.tempData = dict() # dict of weather data for each lat,long {(lat,lon): int}
+        self.api = config.api_key
+        self.generateTempData()
         pass
+
+    def __createApiUrl(self, lat, lon):
+        return f'api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&units=metric&appid={self.api}'
+
+    def generateTempData(self):
+        self.tempData.clear()
+        random.seed(time.time())
+        for i in range(5): # only grab 60
+            lon = random.choice(range(360))-180
+            lat = random.choice(range(180))-90
+            temp = random.choice(range(30))
+            self.tempData[(lat,lon)] = temp
 
     def pullCityNames(self): # return a dict with set number of keys
-        pass
-
-    def createCityURL(self, cityName):
         pass
 
     def grabTemps(self):
         pass
 
 
-# class JsonEdit():
-#     def __init__(self):
-#         pass
+class JsonEdit():
+    def __init__(self):
+        pass
 
 def main():
     dotInfo = DotScraper()
     tempInfo = CityTempScraper()
 
-
-
+    while True:
+        pass
     pass
 
 if __name__ == '__main__':
