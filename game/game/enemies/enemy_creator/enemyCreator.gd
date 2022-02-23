746f3b133cc02e382adbbfaa6fc297012581fbef
Dylan Strong <97559372+TheeLimitlessPower@users.noreply.github.com>
2022-02-23 14:07:12 -0800

    Updating Branches Current Files

--- game/game/enemies/enemy_creator/enemyCreator.gd
+++ game/game/enemies/enemy_creator/enemyCreator.gd
@@ -9,7 +9,10 @@ extends Node
 
 onready var Enemy = find_node("EnemyEntity")
 
+signal health_gone
+
 var enemy
+var preSet = false
 
 func _ready():
 	enemy = Enemy.instance()
@@ -17,18 +20,35 @@ func _ready():
 	add_child(enemy)
 
 func setEnemy(enemyType : String):
-	if(enemyType == "grunt"):
-		enemy.speed *= (1.25)
-		enemy.health *= (0.75)
-		enemy.attack *= (0.75)
-	if(enemyType == "bulky"):
-		enemy.speed *= (0.5)
-		enemy.health *= (2.0)
-		enemy.attack *= (1.0)
-	if(enemyType == "speedy"):
-		enemy.speed *= (1.5)
-		enemy.health *= (0.5)
-		enemy.attack *= (0.5)
+	if(preSet == false):
+		if(enemyType == "grunt"):
+			enemy.speed *= (1.25)
+			enemy.health *= (0.75)
+			enemy.attack *= (0.75)
+			enemy.defense *= (0.75)
+			
+			preSet = true
+		
+		if(enemyType == "bulky"):
+			enemy.speed *= (0.5)
+			enemy.health *= (2.0)
+			enemy.attack *= (1.0)
+			enemy.defense *= (1.5)
+			
+			preSet = true
+			enemy.hasWeakness = true
+		
+		if(enemyType == "speedy"):
+			enemy.speed *= (1.5)
+			enemy.health *= (0.5)
+			enemy.attack *= (0.5)
+			enemy.defense *= (0.5)
+			
+			preSet = true
+			
+		else:
+			pass
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
