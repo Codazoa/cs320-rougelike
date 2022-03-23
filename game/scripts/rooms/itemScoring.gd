56f97b69e87bd007f5ef488420e2374892932ed6
Cody Vernon <cody.vernon@wsu.edu>
2022-03-22 21:17:42 -0700

    Added merchant sprite and initial learning funcs

---
+++ game/scripts/rooms/itemScoring.gd
@@ -0,0 +1,33 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	
+	pass # Replace with function body.
+	
+func ucb1(id):
+	
+	var userID
+	var itemShown
+	var itemGrabbed
+	var alpha
+	pass
+	
+func getUcb1Scores(items):
+	# step through json to populate ucb1 scores data structure
+	# itemID, score
+	# set initial scores to 0
+	pass
+	
+func updateUCB1Score(id):
+	# find id in list
+	# update it with new score
+	
+	pass
+	
+func getTopItems(num):
+	# return id of top ranked items
+	pass
+	
+	
