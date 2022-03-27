10032b08772ec42f6a1d296cf917b12025742e29
Cody Vernon <cody.vernon@wsu.edu>
2022-03-26 19:25:09 -0700

    more work on itemScoreing.gd

--- game/scripts/rooms/itemScoring.gd
+++ game/scripts/rooms/itemScoring.gd
@@ -1,33 +1,24 @@
-extends Node2D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	
-	pass # Replace with function body.
-	
-func ucb1(id):
-	
+static func ucb1(id):
 	var userID
 	var itemShown
 	var itemGrabbed
 	var alpha
+	
 	pass
 	
-func getUcb1Scores(items):
+static func getUcb1Scores(arr):
 	# step through json to populate ucb1 scores data structure
 	# itemID, score
 	# set initial scores to 0
-	pass
+	return  [{"id":1, "score":0 },{"id":2, "score":0 },{"id":3, "score":0 }]
 	
-func updateUCB1Score(id):
+static func updateUCB1Score(arr, id):
 	# find id in list
 	# update it with new score
+	return
 	
-	pass
-	
-func getTopItems(num):
+static func getTopItems(arr, num):
 	# return id of top ranked items
-	pass
+	return []
 	
 	
