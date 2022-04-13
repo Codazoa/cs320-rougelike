7d340049cd8a9d5592c3a20771a16781d9d4db5f
Jack Kelly <jackkharvison@gmail.com>
2022-04-13 13:44:21 -0700

    setting up collision with rooms

--- game/scripts/player_scripts/BaseMove.gd
+++ game/scripts/player_scripts/BaseMove.gd
@@ -1,8 +1,8 @@
 extends KinematicBody2D
 
-const ACCELERATION = 50
-const MAX_SPEED = 200
-const FRICTION  = 400
+const ACCELERATION = 500
+const MAX_SPEED = 80
+const FRICTION  = 500
 
 const ROT_SPEED  = 1
 
@@ -22,13 +22,21 @@ func _physics_process(delta):
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector =  input_vector.normalized()
-	
-	if input_vector != Vector2.ZERO:
-		velocity += input_vector * ACCELERATION * delta 
-		velocity = velocity.clamped(MAX_SPEED * delta)
-		rotation = input_vector.angle()
-	
+#
+#	if input_vector != Vector2.ZERO:
+#		velocity += input_vector * ACCELERATION * delta 
+#		velocity = velocity.clamped(MAX_SPEED * delta)
+#		rotation = input_vector.angle()
+#
+#	else:
+#		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	if(input_vector != Vector2.ZERO):
+		velocity = velocity.move_toward(input_vector*MAX_SPEED, ACCELERATION * delta);
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta);
+	move_and_slide(velocity)
 	
-	move_and_collide(velocity)
+	for i in get_slide_count():
+		var collision = get_slide_collision(i);
+		if(collision.collider.name == "roomShifter_hitbox"):
+			collision.collider.handleCollision(self);
