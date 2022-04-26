17c53b650821f75ef814ac46fe2ac05ca11501bc
Jack Kelly <jackkharvison@gmail.com>
2022-04-25 19:21:33 -0700

    updated go_static function in player.gd

--- game/scripts/player_scripts/Player.gd
+++ game/scripts/player_scripts/Player.gd
@@ -23,13 +23,17 @@ func _ready():
 func go_static():
 	in_static = true
 	body.position = Vector2(playerPos.position.x, playerPos.position.y - 40) 
+	body.rotation = 1.52
 	tailBase.position = Vector2(playerPos.position.x, playerPos.position.y - 65)
+	tailBase.rotation = 1.52
 	tailTip.position = Vector2(playerPos.position.x, playerPos.position.y - 80)  
+	tailTip.rotation = 1.52
 	
 func exit_static():
 	in_static = false
 	
 func _process(delta):
+	
 	if Input.is_mouse_button_pressed(static_toggle):
 		if in_static:
 			exit_static()
